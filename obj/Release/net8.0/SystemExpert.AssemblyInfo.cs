1e74fea123e691b1162723314ea4ab1f7ecaaf70
qwertyprob <eqspertars@gmail.com>
2024-11-23 17:46:49 +0200

    deploy3

--- obj/Release/net8.0/SystemExpert.AssemblyInfo.cs
+++ obj/Release/net8.0/SystemExpert.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemExpert")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b23aacc1d4dacd97e5a43a5341fc79223392cca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97dd9b279df005006455bafb3e3d795feaa55539")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemExpert")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemExpert")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
