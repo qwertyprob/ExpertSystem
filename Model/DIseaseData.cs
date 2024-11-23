0fc7f9302ed93b5ee6834640b822862cb3db3144
qwertyprob <eqspertars@gmail.com>
2024-11-23 16:13:36 +0200

    first commit

---
+++ Model/DIseaseData.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SystemExpert.Model
+{
+    public class DiseaseData
+    {
+        [JsonProperty("связь")]
+        public Dictionary<string, List<string>> Symptoms { get; set; }
+    }
+}
---
+++ Model/SymptomData.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace SystemExpert.Model
+{
+    public class SymptomData
+    {
+        [JsonProperty("симптомы")]
+        public List<string> Symptoms { get; set; }
+    }
+
+}
